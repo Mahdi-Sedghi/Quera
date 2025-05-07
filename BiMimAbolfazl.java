44622b34517ebb95296e9de2bfee7d2721892489
Mahdi-Sedghi <mahdisedghi848484@gmail.com>
2025-05-07 23:36:09 +0330

    0001 answer

---
+++ BiMimAbolfazl.java
@@ -0,0 +1,16 @@
+// package Quera;
+import java.util.Scanner;
+
+public class BiMimAbolfazl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        if (name.indexOf("m") != -1) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+        scanner.close();
+    }
+    
+}
